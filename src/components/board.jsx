f62997c68b023efc89cb467c13719ade831fba8e
bomendez <mendez.bo@husky.neu.edu>
2021-11-10 12:47:22 -0800

    Ships randomly placed

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,95 +1,29 @@
-import React, { Component } from 'react';
-import {Square} from './square';
-import {addShip} from '../actions/addShip';
-import { useState, useDispatch, useSelector } from 'react-redux';
-import {AIRCRAFT_CARRIER, BATTLESHIP, SUBMARINE, DESTROYER, PATROL_BOAT, SIZE_TEN} from './constants';
+import React from "react";
+import { Square } from "./square";
 
 function Board(props) {
-    const dispatch = useDispatch();
-    const shipsOnBoard = useSelector(state => state.BoardReducer.ships);
-    console.log(shipsOnBoard)
-    // const [gridRows, setGridRows] = initializeBoardState();
-    let gridRows = initializeBoardState();
-    putShipsOnBoard();
+  const board = initializeBoardState();
 
-    function initializeBoardState(){
-        let gridRows = [];
-        let id = 0;
-         for (let y = 0; y < 10; y++) {
-             const rowSquares = [];
-             for (let x = 0; x < 10; x++) {
-                rowSquares.push(<Square id={id} x_coord={x} y_coord={y}/>);
-                id++;
-             }
-        gridRows.push(<tr>{rowSquares}</tr>);
-        }
-        console.log(gridRows)
-        return gridRows;
+  function initializeBoardState() {
+    let gridRows = [];
+    let id = 0;
+    for (let y = 0; y < 10; y++) {
+      const rowSquares = [];
+      for (let x = 0; x < 10; x++) {
+        rowSquares.push(<Square id={id} x_coord={x} y_coord={y} />);
+        id++;
+      }
+      gridRows.push(<tr>{rowSquares}</tr>);
     }
+    console.log(gridRows);
+    return gridRows;
+  }
 
-    function putShipsOnBoard(){
-        for (let i = 2; i < AIRCRAFT_CARRIER + 1; i++) {
-            placeOneShip(i)
-            if (i === 3) { // two ships have size 3
-                placeOneShip(i)
-            }
-        }
-        console.log(props)
-    }
-
-    // Helper function to place one ship on board 
-    function placeOneShip(shipSize) {
-        
-        let [randomRow, randomCol] = getRowAndCol(shipSize)
-        // check whether ship placement is valid before placing ship
-        while (!shipPlacementValid(randomRow, randomCol, shipSize)) {
-            let newRowCol = getRowAndCol(shipSize)
-            randomRow = newRowCol[0]
-            randomCol = newRowCol[1]
-        }
-        console.log("add ship", randomRow, randomCol)
-        grid[randomRow][randomCol] = 'ship'
-        console.log("updated ships:", shipsOnBoard)
-        
-        fillRemainingShipSize(randomRow, randomCol, shipSize)
-    }
-
-    // Helper function checking whether a ship can be placed
-    // on given location with specified ship length
-    function shipPlacementValid(row, col, length) {
-        for (let i = col; i < length - 1; i++) {
-            if (shipsOnBoard.some(e => e.x_coord === row && e.y_coord === col)) { return false}
-        }
-        return true
-    }
-
-    // Helper function that takes leftmost cell of ship and
-    // sets state of squares to the right for length of ship
-    function fillRemainingShipSize(row, col, length) {
-        for (let i = col + 1; i < length; i++) {
-            dispatch(addShip(row, i));
-            console.log("fillRemainingShipSize with", row, i)
-            console.log("fillRemainingShips:", shipsOnBoard)
-        }
-    }
-
-    // Helper function to get random row and column values
-    function getRowAndCol(shipSize) {
-        let randomRow = getRandomInteger(9 - shipSize)
-        let randomCol = getRandomInteger(SIZE_TEN)
-        return [randomRow, randomCol]
-    }
-
-    // Helper function that returns a random integer between
-    // 0 and a given maxInt
-    function getRandomInteger(maxInt) {
-        return Math.floor(Math.random() * maxInt);
-    }
-
-    return(
-            <table className="board-class"><tbody>{gridRows}</tbody></table>
-    )
-
+  return (
+    <table className="board-class">
+      <tbody>{board}</tbody>
+    </table>
+  );
 }
 
 export default Board;
--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,20 +1,129 @@
-import { initialState } from "./gameState";
-import {BOARD_CLICK, ADD_SHIPS} from '../actions/constants';
+import { BOARD_CLICK, ADD_SHIPS } from "../actions/constants";
+import { AIRCRAFT_CARRIER, SIZE_TEN } from "../components/constants";
 
-export const BoardReducer = (state = initialState, action) => {
-    if(action.type === BOARD_CLICK 
-        //avoid already clicked squares being added to the list of visited squares
-        && !state.clickedSquares.some(e => e.x_coord === action.payload.x_coord && e.y_coord === action.payload.y_coord)){
-        return {
-                ...state,
-                clickedSquares: state.clickedSquares.concat({x_coord: action.payload.x_coord, y_coord: action.payload.y_coord})
-            }
+function initialStateFunc() {
+  const initialState = {
+    clickedSquares: [],
+    ships: [],
+    player: {
+      currentPlayer: 0,
+    },
+  };
+
+  let gridRows = initializeBoardState();
+  putShipsOnBoard();
+
+  function initializeBoardState() {
+    let gridRows = [];
+    let id = 0;
+    for (let y = 0; y < 10; y++) {
+      const rowSquares = [];
+      for (let x = 0; x < 10; x++) {
+        rowSquares.push(id);
+        id++;
+      }
+      gridRows.push(rowSquares);
     }
-    if(action.type === ADD_SHIPS) {
-        return {
-            ...state,
-            ships: state.ships.concat([{x_coord: action.payload.x_coord, y_coord: action.payload.y_coord}])
-        }
+    console.log(gridRows);
+    return gridRows;
+  }
+
+  function putShipsOnBoard() {
+    for (let i = 2; i < AIRCRAFT_CARRIER + 1; i++) {
+      placeOneShip(i);
+      if (i === 3) {
+        // two ships have size 3
+        placeOneShip(i);
+      }
     }
-    return state
+  }
+
+  // Helper function to place one ship on board
+  function placeOneShip(shipSize) {
+    let [randomRow, randomCol] = getRowAndCol(shipSize);
+    // check whether ship placement is valid before placing ship
+    while (!shipPlacementValid(randomRow, randomCol, shipSize)) {
+      let newRowCol = getRowAndCol(shipSize);
+      randomRow = newRowCol[0];
+      randomCol = newRowCol[1];
+    }
+    console.log("add ship", randomRow, randomCol);
+    gridRows[randomRow][randomCol] = "ship";
+    initialState.ships.push({ x_coord: randomCol, y_coord: randomRow });
+    // console.log("updated ships:", shipsOnBoard);
+
+    fillRemainingShipSize(randomRow, randomCol, shipSize);
+  }
+
+  // Helper function checking whether a ship can be placed
+  // on given location with specified ship length
+  function shipPlacementValid(row, col, length) {
+    for (let i = col; i < length - 1; i++) {
+      if (
+        initialState.ships.some((e) => e.x_coord === row && e.y_coord === col)
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Helper function that takes leftmost cell of ship and
+  // sets state of squares to the right for length of ship
+  function fillRemainingShipSize(row, col, length) {
+    let currCol = col + 1;
+    for (let i = 0; i < length - 1; i++) {
+      gridRows[row][currCol] = "ship";
+      initialState.ships.push({ x_coord: currCol, y_coord: row });
+      currCol++;
+      //   console.log("fillRemainingShipSize with", row, i);
+      //   console.log("fillRemainingShips:", shipsOnBoard);
+    }
+  }
+
+  // Helper function to get random row and column values
+  function getRowAndCol(shipSize) {
+    let randomRow = getRandomInteger(SIZE_TEN);
+    let randomCol = getRandomInteger(9 - shipSize);
+    return [randomRow, randomCol];
+  }
+
+  // Helper function that returns a random integer between
+  // 0 and a given maxInt
+  function getRandomInteger(maxInt) {
+    return Math.floor(Math.random() * maxInt);
+  }
+
+  initialState.board = gridRows;
+  console.log(initialState);
+  return initialState;
 }
+
+// REDUCER STARTS HERE
+export const BoardReducer = (state, action) => {
+  if (state === undefined) {
+    return initialStateFunc();
+  }
+
+  if (
+    action.type === BOARD_CLICK &&
+    //avoid already clicked squares being added to the list of visited squares
+    !state.clickedSquares.some(
+      (e) =>
+        e.x_coord === action.payload.x_coord &&
+        e.y_coord === action.payload.y_coord
+    )
+  ) {
+    return {
+      ...state,
+      clickedSquares: [
+        ...state.clickedSquares,
+        {
+          x_coord: action.payload.x_coord,
+          y_coord: action.payload.y_coord,
+        },
+      ],
+    };
+  }
+  return state;
+};
